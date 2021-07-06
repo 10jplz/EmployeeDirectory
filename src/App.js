b187004c0e473075b702f163820aa9398072e7c3
Jose Contreras <10jplz@Gmail.com>
2021-07-05 19:16:52 -0600

    Worked on appjs and headerjs

--- src/App.js
+++ src/App.js
@@ -3,19 +3,18 @@ import {BrowserRouter as Router, Route } from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import Header from "./components/Header";
 import Wrapper from "./components/Wrapper";
-import API from "./utils/API";
 
 
-console.log(API);
+
 
 function App() {
   return (
     <Router>
       <div>
         <Header>
-          {/* <Wrapper>
-            <Route path="/" component= {Welcome} />
-          </Wrapper> */}
+          <Wrapper>
+          <Route  path="/" component={props => <Welcome {...props} />} />
+          </Wrapper>
         </Header>
       </div>
     </Router>
